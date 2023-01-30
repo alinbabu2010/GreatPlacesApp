a23cd9140d730a32895595738680a0905ac26da6
Alin Babu <alinbabu2010@gmail.com>
2023-01-30 12:41:29 +0530

    Add provider and home screen to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:great_places/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +14,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-          colorScheme: const ColorScheme.light(
-        primary: Colors.indigo,
-        secondary: Colors.amber,
-      )),
-      home: const Placeholder(),
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: Constants.greatPlaces,
+        theme: ThemeData(
+            colorScheme: const ColorScheme.light(
+          primary: Colors.indigo,
+          secondary: Colors.amber,
+        )),
+        home: const PlacesListScreen(),
+      ),
     );
   }
 }
