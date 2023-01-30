c2678b117dac1f159d14d34c49be602ddd98b6c8
Alin Babu <alinbabu2010@gmail.com>
2023-01-30 18:03:25 +0530

    Add function to add place in provider class

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:great_places/data/models/place_location.dart';
 
 import '../data/models/place.dart';
 
@@ -6,4 +9,15 @@ class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
 
   List<Place> get items => [..._items];
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      title: title,
+      location: PlaceLocation(latitude: 0.0, longitude: 0.0),
+      image: image,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+  }
 }
