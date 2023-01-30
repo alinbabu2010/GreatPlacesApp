52661e98ab0dc1eb1bbdb7840faedc25311c093b
Alin Babu <alinbabu2010@gmail.com>
2023-01-30 18:21:48 +0530

    Add places list screen body with lists and also add item widget

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/add_place_screen.dart';
 import 'package:great_places/utils/constants.dart';
+import 'package:great_places/utils/dimensions.dart';
+import 'package:great_places/widgets/places_list_item.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key? key}) : super(key: key);
@@ -19,7 +23,38 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(child: CircularProgressIndicator()),
+      body: Consumer<GreatPlaces>(
+        builder: (context, places, child) => places.items.isEmpty
+            ? child!
+            : Container(
+                margin: const EdgeInsets.only(
+                  top: Dimensions.placesListTopMargin,
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Dimensions.placesListHorizontalPadding,
+                ),
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    final place = places.items[index];
+                    return PlacesListItem(place, () {
+                      // TODO:Go to detail page..
+                    });
+                  },
+                  itemCount: places.items.length,
+                ),
+              ),
+        child: const Center(
+          child: Text(
+            Constants.gotNoPlaces,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: Dimensions.placesListEmptyFontSize,
+              fontWeight: FontWeight.w500,
+              color: Colors.brown,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/places_list_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/data/models/place.dart';
+import 'package:great_places/utils/dimensions.dart';
+
+class PlacesListItem extends StatelessWidget {
+  final Place place;
+  final VoidCallback onClick;
+
+  const PlacesListItem(
+    this.place,
+    this.onClick, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(Dimensions.placeItemPadding),
+      decoration: const BoxDecoration(
+        color: Colors.amberAccent,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.all(
+          Radius.circular(Dimensions.placeItemBorderRadius),
+        ),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: FileImage(place.image),
+        ),
+        title: Text(place.title),
+        onTap: onClick,
+      ),
+    );
+  }
+}
