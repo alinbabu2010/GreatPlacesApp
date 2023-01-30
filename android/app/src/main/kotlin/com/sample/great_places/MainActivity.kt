a0d26eb5ecb4dafe0d107b35f255e3a1c0416ae2
Alin Babu <alinbabu2010@gmail.com>
2023-01-30 11:51:38 +0530

    Add initial project with basic setup

---
+++ android/app/src/main/kotlin/com/sample/great_places/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sample.great_places
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      theme: ThemeData(
+          colorScheme: const ColorScheme.light(
+        primary: Colors.indigo,
+        secondary: Colors.amber,
+      )),
+      home: const Placeholder(),
+    );
+  }
+}
