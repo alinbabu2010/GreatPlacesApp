b15f63f0e65b7139a9ce1f7e8f757f481476562d
Alin Babu <alinbabu2010@gmail.com>
2023-02-01 12:40:44 +0530

    Add function get data from database and refactor code
    Add common function for database setting and update create table query

--- lib/data/local/db_source.dart
+++ lib/data/local/db_source.dart
@@ -1,6 +1,7 @@
+import 'package:great_places/data/local/db_constants.dart';
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
-import 'package:sqflite/sql.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class DatabaseSource {
 
@@ -12,18 +13,27 @@ class DatabaseSource {
     databaseSource ??= DatabaseSource();
   }
 
-  Future<void> insert(String table, Map<String, Object> data) async {
+  Future<Database> _database() async {
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(path.join(dbPath, 'places.db'),
+    return sql.openDatabase(path.join(dbPath, DbConstants.dbName),
         onCreate: (database, version) {
-      database.execute(
-          'CREATE TABLE user_places(id INTEGER PRIMARY KEY, name TEXT, image TEXT)');
+      return database.execute(
+          'CREATE TABLE user_places(id STRING PRIMARY KEY, title TEXT, image TEXT)');
     }, version: 1);
+  }
+
+  Future<void> insert(String table, Map<String, Object> data) async {
+    final sqlDb = await _database();
     await sqlDb.insert(
       table,
       data,
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
+  }
+
+  Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await _database();
+    return db.query(table);
   }
 
 }
