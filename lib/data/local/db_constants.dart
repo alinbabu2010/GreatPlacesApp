95130dc1f6754d96c0786022d40ce9f79ab2864a
Alin Babu <alinbabu2010@gmail.com>
2023-02-01 12:36:02 +0530

    Add constants file for database

---
+++ lib/data/local/db_constants.dart
@@ -0,0 +1,10 @@
+class DbConstants {
+  static const dbName = 'places.db';
+  static const placesTable = "user_places";
+}
+
+class MapKey {
+  static const id = "id";
+  static const title = "title";
+  static const image = "image";
+}
