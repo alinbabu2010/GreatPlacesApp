27b822121585c3e2dd5153b918c2de76493c0f6e
Alin Babu <alinbabu2010@gmail.com>
2023-02-01 10:29:39 +0530

    Add database source instance and inset into db on addPlace function

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:great_places/data/local/db_source.dart';
 import 'package:great_places/data/models/place_location.dart';
 
 import '../data/models/place.dart';
@@ -9,6 +10,8 @@ class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
 
   List<Place> get items => [..._items];
+
+  final databaseSource = DatabaseSource.getInstance();
 
   void addPlace(String title, File image) {
     final newPlace = Place(
@@ -19,5 +22,10 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace);
     notifyListeners();
+    databaseSource.insert("places", {
+      'id': newPlace.id,
+      "title": newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
 }
