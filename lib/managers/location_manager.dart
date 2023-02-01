f473efcbfb5e7d827837e32cd470a44c5edb2ce0
Alin Babu <alinbabu2010@gmail.com>
2023-02-01 17:33:30 +0530

    Add location manager class to handle location services

---
+++ lib/managers/location_manager.dart
@@ -0,0 +1,55 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:great_places/data/models/place_location.dart';
+
+class LocationManger {
+  
+  static const _dummyApiKey = "AIzaSyBg9yn5JtQgKRFbg6FCTy4ewbF24kRuAYI";
+
+  static Future<PlaceLocation> getCurrentUserLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    final position = await Geolocator.getCurrentPosition();
+    return Future.value(
+      PlaceLocation(
+        latitude: position.latitude,
+        longitude: position.longitude,
+      ),
+    );
+  }
+
+  static String generatedLocationPreviewUrl(double latitude, double longitude) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$_dummyApiKey';
+  }
+
+}
