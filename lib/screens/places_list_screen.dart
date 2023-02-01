24eed18bb691652a2f56a8a45ab129f55ead50ae
Alin Babu <alinbabu2010@gmail.com>
2023-02-01 12:44:32 +0530

    Add `FutureBuilder` widget to get products from provider in list screen
    Also add progressbar and error handling

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -4,6 +4,7 @@ import 'package:great_places/screens/add_place_screen.dart';
 import 'package:great_places/utils/constants.dart';
 import 'package:great_places/utils/dimensions.dart';
 import 'package:great_places/widgets/places_list_item.dart';
+import 'package:great_places/widgets/progress_bar.dart';
 import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
@@ -23,35 +24,42 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        builder: (context, places, child) => places.items.isEmpty
-            ? child!
-            : Padding(
-                padding: const EdgeInsets.only(
-                    left: Dimensions.placesListSidePadding,
-                    right: Dimensions.placesListSidePadding,
-                    bottom: Dimensions.placesListBottomPadding),
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    final place = places.items[index];
-                    return PlacesListItem(place, () {
-                      // TODO:Go to detail page..
-                    });
-                  },
-                  itemCount: places.items.length,
-                ),
-              ),
-        child: const Center(
-          child: Text(
-            Constants.gotNoPlaces,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: Dimensions.placesListEmptyFontSize,
-              fontWeight: FontWeight.w500,
-              color: Colors.brown,
-            ),
-          ),
-        ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const ProgressBar()
+                : Consumer<GreatPlaces>(
+                    builder: (context, places, child) => places.items.isEmpty || snapshot.hasError
+                        ? child!
+                        : Padding(
+                            padding: const EdgeInsets.only(
+                                left: Dimensions.placesListSidePadding,
+                                right: Dimensions.placesListSidePadding,
+                                bottom: Dimensions.placesListBottomPadding),
+                            child: ListView.builder(
+                              itemBuilder: (context, index) {
+                                final place = places.items[index];
+                                return PlacesListItem(place, () {
+                                  // TODO:Go to detail page..
+                                });
+                              },
+                              itemCount: places.items.length,
+                            ),
+                          ),
+                    child: Center(
+                      child: Text(
+                        snapshot.hasError ? snapshot.error.toString() : Constants.gotNoPlaces,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: Dimensions.placesListEmptyFontSize,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.brown,
+                        ),
+                      ),
+                    ),
+                  ),
       ),
     );
   }
