82f6f6037a1afff25099b48f0e9fc9db21335127
Alin Babu <alinbabu2010@gmail.com>
2023-02-01 12:41:42 +0530

    Add function to fetch products from database source in provider class

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:great_places/data/local/db_constants.dart';
 import 'package:great_places/data/local/db_source.dart';
 import 'package:great_places/data/models/place_location.dart';
 
@@ -28,4 +29,18 @@ class GreatPlaces with ChangeNotifier {
       'image': newPlace.image.path,
     });
   }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await databaseSource.getData(DbConstants.placesTable);
+    _items = dataList
+        .map((item) => Place(
+              id: item[MapKey.id],
+              title: item[MapKey.title],
+              location: PlaceLocation(latitude: 0.0, longitude: 0.0),
+              image: File(item[MapKey.title]),
+            ))
+        .toList();
+    notifyListeners();
+  }
+
 }
