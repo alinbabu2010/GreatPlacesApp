2e55e47caafc4acfce4a04529ea74135bd142b5e
Alin Babu <alinbabu2010@gmail.com>
2023-02-02 10:42:53 +0530

    Add dotenv initialization on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:great_places/managers/navigation_manager.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/places_list_screen.dart';
 import 'package:great_places/utils/constants.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
