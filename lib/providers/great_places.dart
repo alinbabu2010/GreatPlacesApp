04e2cd68ce85bfaaba37dbda66777acef6202314
Alin Babu <alinbabu2010@gmail.com>
2023-02-03 16:39:47 +0530

    Modify provider to store location in database and refactor code

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:great_places/data/local/db_constants.dart';
 import 'package:great_places/data/local/db_source.dart';
 import 'package:great_places/data/models/place_location.dart';
+import 'package:great_places/managers/location_manager.dart';
 
 import '../data/models/place.dart';
 
@@ -14,33 +15,51 @@ class GreatPlaces with ChangeNotifier {
 
   final databaseSource = DatabaseSource.getInstance();
 
-  Future<void> addPlace(String title, File image) {
+  Future<void> addPlace(
+    String title,
+    File image,
+    PlaceLocation pickedLocation,
+  ) async {
+    pickedLocation.address = await LocationManger.getPlaceAddress(
+      pickedLocation.latitude,
+      pickedLocation.longitude,
+    );
     final newPlace = Place(
       id: DateTime.now().toString(),
       title: title,
-      location: PlaceLocation(latitude: 0.0, longitude: 0.0),
+      location: pickedLocation,
       image: image,
     );
     _items.add(newPlace);
     notifyListeners();
-    return databaseSource.insert(DbConstants.placesTable, {
-      MapKey.id: newPlace.id,
-      MapKey.title: newPlace.title,
-      MapKey.image: newPlace.image.path,
-    });
+    return databaseSource.insert(
+        DbConstants.placesTable, _getDatabaseData(newPlace));
   }
+
+  Map<String, Object> _getDatabaseData(Place place) => {
+        MapKey.id: place.id,
+        MapKey.title: place.title,
+        MapKey.image: place.image.path,
+        MapKey.latitude: place.location.latitude,
+        MapKey.longitude: place.location.longitude,
+        MapKey.address: place.location.address,
+      };
 
   Future<void> fetchAndSetPlaces() async {
     final dataList = await databaseSource.getData(DbConstants.placesTable);
-    _items = dataList
-        .map((item) => Place(
-              id: item[MapKey.id],
-              title: item[MapKey.title],
-              location: PlaceLocation(latitude: 0.0, longitude: 0.0),
-              image: File(item[MapKey.image]),
-            ))
-        .toList();
+    _items = dataList.map((item) => generatePlace(item)).toList();
     notifyListeners();
   }
+
+  Place generatePlace(Map<String, dynamic> item) => Place(
+        id: item[MapKey.id],
+        title: item[MapKey.title],
+        location: PlaceLocation(
+          latitude: item[MapKey.latitude],
+          longitude: item[MapKey.longitude],
+          address: item[MapKey.address],
+        ),
+        image: File(item[MapKey.image]),
+      );
 
 }
